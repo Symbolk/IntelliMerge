431212c946f0c0f0c5fea19b78ae4ac7d23a43a3
Symbolk <symbolk@163.com>
2019-10-09 21:03:57 +0800

    [opt] Merge modifiers as part of the original signature, to avoid in-line conflicts.

--- src/main/java/edu/pku/intellimerge/core/GraphBuilderV2.java
+++ src/main/java/edu/pku/intellimerge/core/GraphBuilderV2.java
@@ -586,7 +586,13 @@ public class GraphBuilderV2 implements Callable<Graph<SemanticNode, SemanticEdge
           for (VariableDeclarator field : fd.getVariables()) {
             displayName = field.getNameAsString().trim();
             qualifiedName = qualifiedTypeName + "." + displayName;
-            originalSignature = (field.getTypeAsString() + " " + field.getNameAsString()).trim();
+            originalSignature =
+                (modifiers.stream().collect(Collectors.joining(" "))
+                        + " "
+                        + field.getTypeAsString()
+                        + " "
+                        + field.getNameAsString())
+                    .trim();
             body =
                 field.getInitializer().isPresent()
                     ? field.getTokenRange().get().toString().replaceFirst(displayName, "") + ";"
@@ -684,6 +690,8 @@ public class GraphBuilderV2 implements Callable<Graph<SemanticNode, SemanticEdge
           if (startIndex <= endIndex) {
             originalSignature = temp.substring(startIndex, endIndex);
           }
+          originalSignature =
+              modifiers.stream().collect(Collectors.joining(" ")) + " " + originalSignature;
           cdNode.setOriginalSignature(originalSignature);
 
           graph.addVertex(cdNode);
@@ -790,7 +798,8 @@ public class GraphBuilderV2 implements Callable<Graph<SemanticNode, SemanticEdge
           } else {
             originalSignature = md.getDeclarationAsString(false, true, true);
           }
-
+          originalSignature =
+              modifiers.stream().collect(Collectors.joining(" ")) + " " + originalSignature;
           mdNode.setOriginalSignature(originalSignature);
 
           mdNode.followingEOL = getFollowingEOL(md);
@@ -907,10 +916,10 @@ public class GraphBuilderV2 implements Callable<Graph<SemanticNode, SemanticEdge
           AssignExpr parentAssign = (AssignExpr) parent;
           if (parentAssign.getTarget().equals(fieldAccessExpr)) {
             writeFieldExprs.add(fieldAccessExpr);
-          }else{
+          } else {
             readFieldExprs.add(fieldAccessExpr);
           }
-        }else{
+        } else {
           readFieldExprs.add(fieldAccessExpr);
         }
       }
